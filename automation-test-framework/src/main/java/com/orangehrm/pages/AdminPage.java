2c81b3a10f4de2c2a993879c54f7b1d86dfd5462
siddharth5203 <siddharthbhavsar49@gmail.com>
2020-01-12 10:25:43 +0530

    webdriverwait method added

--- automation-test-framework/src/main/java/com/orangehrm/pages/AdminPage.java
+++ automation-test-framework/src/main/java/com/orangehrm/pages/AdminPage.java
@@ -3,6 +3,8 @@ package com.orangehrm.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.orangehrm.comman.WebDriverFactory;
 
@@ -11,13 +13,19 @@ public class AdminPage {
 	@FindBy(id="btnAdd")
 	WebElement addUserButton;
 	
+	WebDriverWait wait = new WebDriverWait(WebDriverFactory.getWebDriver(), 30);
+	
+	public AdminPage isAdminPageLoaded() {
+		wait.until(ExpectedConditions.elementToBeClickable(addUserButton));
+		return this;
+	}
 	public AdminPage() {
 		PageFactory.initElements(WebDriverFactory.getWebDriver(), this);
 	}
 	
 	public AddUserPage navigateToAddUserPage() {
 		addUserButton.click();
-		return (new AddUserPage());
+		return (new AddUserPage().isAddUserPageLoaded());
 	}
 	
 	public void verifyAdminPage() {
